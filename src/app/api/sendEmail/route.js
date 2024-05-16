1c1d0177ea1749c753a792b90b81da5caf6722f5
Ünal İSİ <unal213027@ogr.duzce.edu.tr>
2024-05-16 12:29:55 +0300

    mailSend

---
+++ src/app/api/sendEmail/route.js
@@ -0,0 +1,37 @@
+import { NextResponse } from 'next/server';
+import nodemailer from 'nodemailer'
+
+export async function POST(request) {
+    try {
+        const { subject, message } = await request.json();
+
+        const transporter = nodemailer.createTransport({
+            service: 'zoho',
+            host: 'smtp.sendgrid.net',
+            port: 587,
+            secure: true,
+            auth: {
+                user: 'apikey',
+                pass: process.env.SENDGRID_API_KEY
+            }
+        })
+
+        const mailOption = {
+            from: 'unalisi.dev@gmail.com',
+            to: 'ichbinunal@gmail.com',
+            subject: "Send Email Tutorial",
+            html: `
+        <h3>Hello Augustine</h3>
+        <li> title: ${subject}</li>
+        <li> message: ${message}</li> 
+        `
+        }
+
+        await transporter.sendMail(mailOption)
+
+        return NextResponse.json({ message: "Email Sent Successfully" }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ message: "Failed to Send Email" }, { status: 500 })
+    }
+}
+
